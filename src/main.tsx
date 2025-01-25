aea8e6df3c2920ad3abef128bd234817ba7c853e
ShaksterNano <54268387+shaksternano@users.noreply.github.com>
2025-01-25 22:50:33 +0000

    feat: add authentication

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import {Amplify} from "aws-amplify";
+import {Authenticator} from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 import outputs from "../amplify_outputs.json";
 
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <App/>
+        <Authenticator>
+            <App/>
+        </Authenticator>
     </React.StrictMode>
 );
