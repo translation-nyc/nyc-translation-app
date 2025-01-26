4b5ad76262df98232b4e659e9037cc2ec5abfabc
ShaksterNano <54268387+shaksternano@users.noreply.github.com>
2025-01-26 14:03:50 +0000

    feat: implement translation box

---
+++ src/components/TranslationBox.tsx
@@ -0,0 +1,54 @@
+export interface TranslationBoxProps {
+    texts: TranslationTextEntry[];
+}
+
+export interface TranslationTextEntry {
+    text: string;
+    type: TranslationTextType;
+}
+
+export type TranslationTextType = "native" | "foreign" | "translated";
+
+function TranslationBox(props: TranslationBoxProps) {
+    return (
+        <div className="w-xl pt-3 p-4 border-gray-500 border-1 rounded-xl bg-gray-200">
+            <div className="relative">
+                <p className="absolute top-0 right-0 text-gray-500">
+                    Transcription
+                </p>
+            </div>
+            <div className="mt-10">
+                {props.texts === undefined || props.texts.length === 0
+                    ?
+                    <p className="text-gray-500">
+                        Waiting to begin speech...
+                    </p>
+                    :
+                    props.texts.map((text, index) => {
+                        let className = "";
+                        switch (text.type) {
+                            case "native":
+                                className = "mt-4";
+                                break;
+                            case "foreign":
+                                className = "mt-4 text-gray-500";
+                                break;
+                        }
+                        return <>
+                            {text.type === "translated" &&
+                                <p className="mt-4 text-blue-600">
+                                    (TRANSLATION)
+                                </p>
+                            }
+                            <p key={index} className={className}>
+                                {text.text}
+                            </p>
+                        </>
+                    })
+                }
+            </div>
+        </div>
+    );
+}
+
+export default TranslationBox;
