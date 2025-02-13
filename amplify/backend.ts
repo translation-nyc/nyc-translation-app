ead85ebe21895bce5a4aefd758c19d073fb7e4d2
ShaksterNano <54268387+shaksternano@users.noreply.github.com>
2025-02-13 17:32:53 +0000

    feat: authenticate transcription with session credentials

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,18 +1,15 @@
 import {defineBackend} from "@aws-amplify/backend";
 import {PolicyStatement} from "aws-cdk-lib/aws-iam";
 import {auth} from "./auth/resource";
-import {data} from "./data/resource";
-import {getTranscribeDetails} from "./functions/transcribe-details/resource";
 
 const backend = defineBackend({
     auth,
-    data,
-    getTranscribeDetails,
 });
 
 backend.auth.resources.authenticatedUserIamRole.addToPrincipalPolicy(
     new PolicyStatement({
         actions: [
+            "transcribe:StartStreamTranscriptionWebSocket",
             "translate:TranslateText",
             "polly:SynthesizeSpeech",
         ],
@@ -24,6 +21,10 @@ backend.addOutput({
     custom: {
         Predictions: {
             convert: {
+                transcription: {
+                    proxy: false,
+                    region: backend.auth.stack.region,
+                },
                 translateText: {
                     proxy: false,
                     region: backend.auth.stack.region,
