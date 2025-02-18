6dcb9b9d529ab2ce5bdc2cd98f9a9f5a5ee03c2e
ShaksterNano <54268387+shaksternano@users.noreply.github.com>
2025-02-18 14:59:09 +0000

    feat: fix: remove backend client usage

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import {Amplify} from "aws-amplify";
-import {generateClient} from "aws-amplify/api";
-import type {Schema} from "../amplify/data/resource.ts";
 import outputs from "../amplify_outputs.json";
 import {Authenticator} from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
@@ -15,8 +13,6 @@ Amplify.configure({
     ...Amplify.getConfig(),
     Predictions: outputs.custom.Predictions,
 });
-
-export const client = generateClient<Schema>();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
