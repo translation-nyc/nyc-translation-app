14045ba7b406bf5deb95da06387203d24a3fdf6b
ShaksterNano <54268387+shaksternano@users.noreply.github.com>
2025-02-24 23:44:35 +0000

    feat: set transcript text to white

--- src/components/Transcript.tsx
+++ src/components/Transcript.tsx
@@ -1,5 +1,3 @@
-import {TextAreaField} from "@aws-amplify/ui-react";
-
 interface TranscriptProps {
     transcript: string;
 }
@@ -8,17 +6,8 @@ function Transcript(props: TranscriptProps) {
     // Transcription set as aws sends it in
     // Text area field maybe not best to use for transcription?
     return (
-        <div className="flex-1 bg-white dark:bg-gray-800 rounded-lg shadow-lg overflow-hidden">
-            <TextAreaField
-                descriptiveText="This is where transcription will be generated"
-                label="Transcription"
-                labelHidden={true}
-                placeholder="This is where transcription will be generated"
-                rows={20}
-                isReadOnly={true}
-                variation="quiet"
-                value={props.transcript}
-            />
+        <div className="flex-1 bg-white dark:bg-gray-800 rounded-lg shadow-lg overflow-hidden p-4 text-white">
+            {props.transcript}
         </div>
     );
 }
