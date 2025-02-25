88db60fc8b91697407e0adde8c5fa5611632b6bc
ShaksterNano <54268387+shaksternano@users.noreply.github.com>
2025-02-25 12:54:53 +0000

    perf: do not transcribe silence

--- src/utils/pcm-processor.ts
+++ src/utils/pcm-processor.ts
@@ -1,14 +1,37 @@
+const VOLUME_THRESHOLD = 0.01;
+const MAX_SILENCE_DURATION_SECONDS = 2;
+
 class PcmProcessor extends AudioWorkletProcessor {
+
+    private silenceCountDown = 0;
 
     // noinspection JSUnusedGlobalSymbols
     process(inputs: Float32Array[][]): boolean {
         if (inputs[0].length > 0) {
             const float32Data = inputs[0][0];
-            const uint8Buffer = float32ToUint8Pcm(float32Data);
-            this.port.postMessage(uint8Buffer);
+            const volume = calculateRootMeanSquare(float32Data);
+            if (volume >= VOLUME_THRESHOLD) {
+                const uint8Buffer = float32ToUint8Pcm(float32Data);
+                this.port.postMessage(uint8Buffer);
+                this.silenceCountDown = MAX_SILENCE_DURATION_SECONDS;
+            } else if (this.silenceCountDown > 0) {
+                const uint8Buffer = float32ToUint8Pcm(float32Data);
+                this.port.postMessage(uint8Buffer);
+                const samples = float32Data.length;
+                this.silenceCountDown -= calculateChunkDuration(samples, sampleRate);
+                this.silenceCountDown = Math.max(0, this.silenceCountDown);
+            }
         }
         return true;
     }
+}
+
+function calculateRootMeanSquare(input: Float32Array): number {
+    let sum = 0;
+    for (let i = 0; i < input.length; i++) {
+        sum += input[i] * input[i];
+    }
+    return Math.sqrt(sum / input.length);
 }
 
 function float32ToUint8Pcm(input: Float32Array): ArrayBuffer {
@@ -18,6 +41,10 @@ function float32ToUint8Pcm(input: Float32Array): ArrayBuffer {
         output.setInt16(i * 2, sample * 32767, true);
     }
     return output.buffer;
+}
+
+function calculateChunkDuration(samples: number, sampleRate: number): number {
+    return samples / sampleRate;
 }
 
 registerProcessor("pcm-processor", PcmProcessor);
