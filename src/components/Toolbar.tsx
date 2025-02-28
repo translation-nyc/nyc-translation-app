63f0d38d2cef7a606b121ce948ee4beb8db3ae31
Charles Suddens-Spiers <charlie.suddens-spiers@live.com>
2025-02-28 11:49:41 +0000

    chore: remove unused import

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,13 +1,14 @@
-import { Button, ButtonGroup, useAuthenticator } from "@aws-amplify/ui-react";
+import { Button, ButtonGroup, useAuthenticator, SliderField,  } from "@aws-amplify/ui-react";
 import { ProfileIcon } from "../assets/icons";
-import { Moon, Sun } from "lucide-react";
-import { useTheme } from "../hooks/useTheme"; 
+import { Moon, Sun, HelpCircle, ZoomIn, ZoomOut } from "lucide-react";
+import { useTheme } from "../hooks/useTheme";
+import { useState } from "react";
 import "../styles/Toolbar.css";
 
 function Toolbar() {
     const { signOut } = useAuthenticator();
-    const { theme, cycleTheme } = useTheme(); // Use the hook
-
+    const { theme, cycleTheme } = useTheme();
+    
     return (
         <header className="navbar flex items-center justify-between p-4 bg-white dark:bg-gray-900 border-b border-gray-200 dark:border-gray-700">
             <div className="flex items-center space-x-4">
@@ -17,14 +18,36 @@ function Toolbar() {
             </div>
 
             <div className="flex items-center space-x-2">
-
+                {/* Text Size Slider */}
+                <div className="flex items-center space-x-2 ml-4">
+                    <span className="text-sm">A</span>
+                    <SliderField
+                        label="Text Size Slider"
+                        outerStartComponent={
+                            <ZoomOut />
+                        }
+                        outerEndComponent={
+                            <ZoomIn />
+                        }
+                        min={8}
+                        max={24}
+                        step={2}
+                        />
+                    <span className="text-lg">A</span>
+                </div>
+                
                 <ButtonGroup size="small">
                     {/* Dark mode toggle button */}
                     <Button onClick={cycleTheme} className="dark-mode p-2">
                         {theme === "light" && <Sun className="w-5 h-5 text-yellow-500" />}
                         {theme === "dark" && <Moon className="w-5 h-5 text-black-200" />}
                     </Button>
-
+                    
+                    {/* Help Button */}
+                    <Button className="p-2">
+                        <HelpCircle className="w-5 h-5 text-blue-500" />
+                    </Button>
+                    
                     {/* Logout button */}
                     <Button className="logout-button" size="small" onClick={signOut}>
                         <ProfileIcon className="mr-2 h-4 w-4" />
@@ -32,6 +55,7 @@ function Toolbar() {
                     </Button>
                 </ButtonGroup>
             </div>
+
         </header>
     );
 }
