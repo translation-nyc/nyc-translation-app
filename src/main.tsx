e193d8055031e58940f1c26842e0d548adfa9005
Charles Suddens-Spiers <charlie.suddens-spiers@live.com>
2025-03-09 19:16:08 +0000

    fix: added identity pool to config

--- src/main.tsx
+++ src/main.tsx
@@ -10,11 +10,11 @@ import './styles/Text.css';
 Amplify.configure(outputs);
 
 Amplify.configure({
-    ...Amplify.getConfig(),
     Auth: {
         Cognito: {
             userPoolId: 'eu-west-2_8YnuTx4Co',
             userPoolClientId: '1v5l3bj1jjpuvtj3qttb98aseb',
+            identityPoolId: 'eu-west-2:d829239f-fb6f-4627-bdba-a32d5d1f71eb'
         }
     },
     Predictions: outputs.custom.Predictions,
