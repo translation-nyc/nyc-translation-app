4b30a35a3b58fe9c02280a8761f4b430f32911a8
Charles Suddens-Spiers <charlie.suddens-spiers@live.com>
2025-03-10 13:04:29 +0000

    fix: removed broken auth config

--- src/main.tsx
+++ src/main.tsx
@@ -10,18 +10,12 @@ import './styles/Text.css';
 Amplify.configure(outputs);
 
 Amplify.configure({
-    Auth: {
-        Cognito: {
-            userPoolId: 'eu-west-2_8YnuTx4Co',
-            userPoolClientId: '1v5l3bj1jjpuvtj3qttb98aseb',
-            identityPoolId: 'eu-west-2:d829239f-fb6f-4627-bdba-a32d5d1f71eb',
-        }
-    },
+    ...Amplify.getConfig(),
     Predictions: outputs.custom.Predictions,
 });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>
+	<React.StrictMode>
+		<App/>
+	</React.StrictMode>
 );
