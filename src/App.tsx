358822bc04390d96109db4395fc740b2ac058430
A-Gully <agallul@yahoo.com>
2025-03-20 04:12:05 +0000

    feat: add PopUpProvider in order to facilitate popups

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import Toolbar from "./components/Toolbar";
 import TranscriptionInterface from "./components/TranscriptionInterface.tsx";
+import {PopupProvider} from "./components/Popup.tsx";
 
 function App() {
     return (
         <main className="w-screen h-screen flex flex-col min-h-screen bg-gray-50 dark:bg-gray-900 overflow-hidden">
+            <PopupProvider>
             <Toolbar/>
             <TranscriptionInterface/>
+            </PopupProvider>
         </main>
     );
 }
